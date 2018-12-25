c85f2773c8ad09c53bdd82486d6b4142cb745de0
SushilBajracharya <bajracharya.sushil01@gmail.com>
2018-12-25 22:47:57 +0545

    Updates

---
+++ precache-manifest.fae5a79829f47abbfa20a565ae46e64a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "56c644c706b33cd4b6c0",
+    "url": "/react-gh-pages/static/js/main.56c644c7.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/react-gh-pages/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "56c644c706b33cd4b6c0",
+    "url": "/react-gh-pages/static/css/main.1fbec301.chunk.css"
+  },
+  {
+    "revision": "19915b6b7c673dee0b60b8c970d885ac",
+    "url": "/react-gh-pages/index.html"
+  }
+];
